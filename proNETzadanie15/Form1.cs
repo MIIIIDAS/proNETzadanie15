fac050242dc1c6e87b4a953ae97e8289dc3d437c
MIIIIDAS <berentadrian@gmail.com>
2024-06-02 15:24:23 +0200

    Dodaj pliki projektów.

---
+++ proNETzadanie15/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace SinusoidPlotter
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            PlotSinusoid();
+        }
+
+        private void PlotSinusoid()
+        {
+            // Ustawienia wykresu
+            chart.Series.Clear();
+            chart.ChartAreas[0].AxisX.Minimum = 0;
+            chart.ChartAreas[0].AxisX.Maximum = 2 * Math.PI;
+            chart.ChartAreas[0].AxisY.Minimum = -1;
+            chart.ChartAreas[0].AxisY.Maximum = 1;
+            chart.ChartAreas[0].AxisX.Title = "x";
+            chart.ChartAreas[0].AxisY.Title = "sin(x)";
+
+            // Dodawanie serii do wykresu
+            Series series = chart.Series.Add("Sinusoid");
+            series.ChartType = SeriesChartType.Line;
+            series.Color = System.Drawing.Color.Blue;
+
+            // Dodawanie punktów do serii (wartości funkcji sin(x))
+            double step = 0.1;
+            for (double x = 0; x <= 2 * Math.PI; x += step)
+            {
+                series.Points.AddXY(x, Math.Sin(x));
+            }
+        }
+    }
+}
---
+++ proNETzadanie15/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+// zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+// powiązane z zestawem.
+[assembly: AssemblyTitle("proNETzadanie15")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("proNETzadanie15")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Ustawienie elementu ComVisible na wartość false sprawia, że typy w tym zestawie są niewidoczne
+// dla składników COM. Jeśli potrzebny jest dostęp do typu w tym zestawie z
+// COM, ustaw wartość true dla atrybutu ComVisible tego typu.
+[assembly: ComVisible(false)]
+
+// Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
+[assembly: Guid("34eb021e-e32e-49a5-b84b-943bd1470007")]
+
+// Informacje o wersji zestawu zawierają następujące cztery wartości:
+//
+//      Wersja główna
+//      Wersja pomocnicza
+//      Numer kompilacji
+//      Poprawka
+//
+// Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+// przy użyciu symbolu „*”, tak jak pokazano poniżej:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
